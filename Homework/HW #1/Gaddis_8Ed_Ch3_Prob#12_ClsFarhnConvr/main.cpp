979703839f8d4d42be032a864862bbbe98a2c404
penaandrew12 <penaandrew12@yahoo.com>
2017-08-29 12:21:21 -0700

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Homework/HW #1/Gaddis_8Ed_Ch3_Prob#12_ClsFarhnConvr/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on August 29th, 2017, 12:00 PM
+ * Purpose: Celsius to Fahrenheit Conversion 
+ */
+
+//System Libraries
+#include <iostream> //Input - Output Library
+
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+// Execution begins here!
+int main(int argc, char** argv) {
+    //Declare Variables
+
+    //Initialize variables
+
+    //Input data
+
+    //Map Inputs to Outputs or process the data
+
+    //Output the transformed data
+
+    //Exit stage right!
+    return 0;
+}
