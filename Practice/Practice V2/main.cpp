76296664ee698e477f88b5fc64409614765fa023
penaandrew12 <penaandrew12@yahoo.com>
2017-11-05 23:38:00 -0800

    Signed-off-by: penaandrew12 <penaandrew12@yahoo.com>

---
+++ Practice/Practice V2/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Pena
+ * Created on August 29th, 2017, 12:00 PM
+ * Purpose: CPP Template  
+ */
+
+//System Libraries
+#include <iostream> //Input - Output Library
+
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+// Execution begins here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    int x=25;
+    int y=20;
+    //Initialize variables
+
+    //Input data
+
+    //Map Inputs to Outputs or process the data
+
+    //Output the transformed data
+    cout<<&x<<endl;
+    cout<<&y<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
